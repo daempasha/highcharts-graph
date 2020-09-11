a24d28ea407ae6d37df81d50f9ed4928a493dc2c
daempasha <daempasha@gmail.com>
2020-09-11 23:46:34 +0100

    Built project

---
+++ build/precache-manifest.91b9586ac5bd1fe64429f1bcbdfac49b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79001439d88b778b7f68260704d3d028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f1289848ae51d298b8",
+    "url": "/static/css/2.9f4e3c6f.chunk.css"
+  },
+  {
+    "revision": "cd5b24752bb7ddb3fdea",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40f1289848ae51d298b8",
+    "url": "/static/js/2.7b8bd523.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7b8bd523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5b24752bb7ddb3fdea",
+    "url": "/static/js/main.5965c934.chunk.js"
+  },
+  {
+    "revision": "c754fe7e99f332becd27",
+    "url": "/static/js/runtime-main.b9087d76.js"
+  }
+]);
