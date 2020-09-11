2b7410a705f87711ecf3e8ba5b542954f3f26476
daempasha <daempasha@gmail.com>
2020-09-11 23:50:34 +0100

    Updates

---
+++ precache-manifest.72eabd79c17a5bb133a77161ede8c020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888fe862c85c5340890157cf9e9f5428",
+    "url": "/highcharts-graphs/index.html"
+  },
+  {
+    "revision": "40f1289848ae51d298b8",
+    "url": "/highcharts-graphs/static/css/2.9f4e3c6f.chunk.css"
+  },
+  {
+    "revision": "6ba73963e6f043b1394d",
+    "url": "/highcharts-graphs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40f1289848ae51d298b8",
+    "url": "/highcharts-graphs/static/js/2.7b8bd523.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/highcharts-graphs/static/js/2.7b8bd523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba73963e6f043b1394d",
+    "url": "/highcharts-graphs/static/js/main.d84dbe20.chunk.js"
+  },
+  {
+    "revision": "f18731872c34388ccc4a",
+    "url": "/highcharts-graphs/static/js/runtime-main.3b2703c5.js"
+  }
+]);
