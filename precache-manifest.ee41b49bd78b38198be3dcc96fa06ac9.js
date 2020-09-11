f66afa52ba8a108731faded0f1aaf6dfadeec144
daempasha <daempasha@gmail.com>
2020-09-12 00:22:28 +0100

    Updates

---
+++ precache-manifest.ee41b49bd78b38198be3dcc96fa06ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8263d857db60bb66991225c172499e2",
+    "url": "/highcharts-graph/index.html"
+  },
+  {
+    "revision": "40f1289848ae51d298b8",
+    "url": "/highcharts-graph/static/css/2.9f4e3c6f.chunk.css"
+  },
+  {
+    "revision": "3982520d1a32aa4b87e8",
+    "url": "/highcharts-graph/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "40f1289848ae51d298b8",
+    "url": "/highcharts-graph/static/js/2.7b8bd523.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/highcharts-graph/static/js/2.7b8bd523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3982520d1a32aa4b87e8",
+    "url": "/highcharts-graph/static/js/main.2a7969d4.chunk.js"
+  },
+  {
+    "revision": "1a57ecb0bf7badaa521f",
+    "url": "/highcharts-graph/static/js/runtime-main.237d82cd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d81cbdb685ec58488a62b3fac9631d3e.js"
+  "/highcharts-graph/precache-manifest.ee41b49bd78b38198be3dcc96fa06ac9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/highcharts-graph/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
